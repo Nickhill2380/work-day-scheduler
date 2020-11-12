27d443bb857bee3c74e0482a570bb6d75e6c6eb0
Nick Hill <nickhill2380@gmail.com>
2020-11-12 09:52:02 -0500

    added text fields started save button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,40 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format('dddd, MMMM Do') );
 });
 
+$("#taskInput").on("click", function(){
+    var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+    .addClass("description")
+    .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+
+$("#saveBtn").on("click", function() {
+
+    var text = $(this)
+    .closest(".taskArea")
+    
+    var time = $(this)
+    .closest("taskTime")
+
+    var tasks = {
+        task: text,
+        dueTime: time
+
+    }
+
+    saveTask = function() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+    
+
+});
+
+
+
